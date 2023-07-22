651d79d097ddabdd17a584c37b660bf3b811d22a
l-Spook-l <uaspookua@gmail.com>
2023-07-22 18:24:55 +0300

    Implemented database record retrieval for a specific user

--- Bot/data_base.py
+++ Bot/data_base.py
@@ -34,3 +34,13 @@ async def sql_add_command(state):
             'INSERT INTO user_settings (user, DK021_2015, Status, Procurement_type, Region, Dispatch_time, Email) VALUES (?, ?, ?, ?, ?, ?, ?)',
             tuple(data.values()))
         base.commit()
+
+
+async def sql_read(message):
+    print('databse2: ', message)
+    # перебираем все данные по таблицам в виде списка
+    for ret in cur.execute(f'SELECT * FROM user_settings WHERE user == {message.from_user.id}').fetchall():
+        print('ret', ret)
+        # разбираем таблицу по столбцам
+        await bot.send_message(message.from_user.id,
+                               f'ДК021:2015: {ret[2]}\nСтатус: {ret[3]}\nВид закупівлі: {ret[4]}\nРегіон: {ret[5]}\nЧас відправки: {ret[6]}\nПошта: {ret[7]}')
