b23953ea6d5421564dc2dc73a6b8029fe4c304e9
l-Spook-l <uaspookua@gmail.com>
2023-07-30 20:36:43 +0300

    Properly configured button display based on specific actions

--- Telegram_bot/client.py
+++ Telegram_bot/client.py
@@ -25,10 +25,10 @@ class FSMClient(StatesGroup):
 
 # @dp.message_handler(commands=['start'], state=None)
 async def commands_start(message: types.Message):
-    global ID
-    ID = message.from_user.id
-    if message.from_user.id != 6197309171:
-        await message.answer("Вітаю, оберіть, що потрібно зробити", reply_markup=action_menu_markup)
+    # global ID
+    # ID = message.from_user.id
+    # if message.from_user.id != 6197309171:
+    await message.answer("Вітаю, оберіть, що потрібно зробити", reply_markup=action_menu_markup)
     print(message.from_user.id)
     # Замовник    # Учасник    # Закупівельник    # ДК021:2015
     # Статус    # Вид закупівлі    # Регіон    # Очікувана вартість
@@ -37,96 +37,90 @@ async def commands_start(message: types.Message):
 
 # @dp.message_handler(commands=['Додати запит'], state=None)
 async def create_new_request(message: types.Message):
-    if message.from_user.id == ID:  # если id админа
-        await FSMClient.DK021_2015.set()  # для ожидания ввода
-        await message.answer('Введіть код ДК021:2015', reply_markup=skip_cancel_markup)
+    # if message.from_user.id == ID:  # если id админа
+    await FSMClient.DK021_2015.set()  # для ожидания ввода
+    await message.answer('Введіть код ДК021:2015', reply_markup=skip_cancel_markup)
 
 
 # выход из состояний (команда для отмены)
 # @dp.register_message_handler(state="*", commands='отмена')  # для команды
 # @dp.register_message_handler(Text(equals='отмена', ignore_case=True), state="*")  # для обычного текста
 async def cancel_handler(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        current_state = await state.get_state()
-        if current_state is None:
-            return
-        await state.finish()
-        await message.reply('Додавання нового запиту скасовано')
+    # if message.from_user.id == ID:
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('Додавання нового запиту скасовано', reply_markup=action_menu_markup)
 
 
 # @dp.message_handler(state=FSMClient.DK021_2015)
 async def DK021_2015(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:  # работа со словарем машины состояний
-            # сохраняем в словарь машины состояния
-            print('DK021_2015 message.text', message.text)
-            data['user'] = ID
-            data['DK021_2015'] = message.text
-            print('data1', data)
-        await FSMClient.next()  # для ожидания ввода
-        await message.answer('Введіть статус')
+    # if message.from_user.id == ID:
+    async with state.proxy() as data:  # работа со словарем машины состояний
+        print('DK021_2015 message.text', message.text)
+        data['user'] = message.from_user.id
+        data['DK021_2015'] = message.text
+        print('data1', data)
+    await FSMClient.next()  # для ожидания ввода
+    await message.answer('Введіть статус')
 
 
 # @dp.message_handler(state=FSMClient.Status)
 async def status(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:  # работа со словарем машины состояний
-            # сохраняем в словарь машины состояния
-            data['Status'] = message.text.lower()
-            print('data2', data)
-        await FSMClient.next()  # для ожидания ввода
-        await message.answer('Введіть вид закупівлі')
+    # if message.from_user.id == ID:
+    async with state.proxy() as data:  # работа со словарем машины состояний
+        data['Status'] = message.text.lower()
+        print('data2', data)
+    await FSMClient.next()  # для ожидания ввода
+    await message.answer('Введіть вид закупівлі')
 
 
 # @dp.message_handler(state=FSMClient.Procurement_type)
 async def procurement_type(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:  # работа со словарем машины состояний
-            # сохраняем в словарь машины состояния
-            data['Procurement_type'] = message.text.lower()
-            print('data3', data)
-        await FSMClient.next()  # для ожидания ввода
-        await message.answer('Оберіть потрібний регіон')
+    # if message.from_user.id == ID:
+    async with state.proxy() as data:  # работа со словарем машины состояний
+        data['Procurement_type'] = message.text.lower()
+        print('data3', data)
+    await FSMClient.next()  # для ожидания ввода
+    await message.answer('Оберіть потрібний регіон')
 
 
 # @dp.message_handler(state=FSMClient.Region)
 async def region(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:  # работа со словарем машины состояний
-            # сохраняем в словарь машины состояния
-            data['Region'] = message.text
-            print('data4', data)
-        await FSMClient.next()
-        await message.answer('Введіть час отправлення повідомлення на електронну пошту')
+    # if message.from_user.id == ID:
+    async with state.proxy() as data:  # работа со словарем машины состояний
+        data['Region'] = message.text
+        print('data4', data)
+    await FSMClient.next()
+    await message.answer('Введіть час отправлення повідомлення на електронну пошту')
 
 
 async def dispatch_time(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:  # работа со словарем машины состояний
-            # Удаление всех символов кроме цифр
-            cleaned_time = re.sub(r'\D', '', message.text)
-            # Добавление символа ":" после первых двух цифр
-            formatted_time = cleaned_time[:2] + ":" + cleaned_time[2:]
-            # сохраняем в словарь машины состояния
-            data['Dispatch_time'] = formatted_time
-            print('data5', data)
-        await FSMClient.next()
-        await message.answer('Введіть адрес електронної пошти')
+    # if message.from_user.id == ID:
+    async with state.proxy() as data:  # работа со словарем машины состояний
+        # Удаление всех символов кроме цифр
+        cleaned_time = re.sub(r'\D', '', message.text)
+        # Добавление символа ":" после первых двух цифр
+        formatted_time = cleaned_time[:2] + ":" + cleaned_time[2:]
+        # сохраняем в словарь машины состояния
+        data['Dispatch_time'] = formatted_time
+        print('data5', data)
+    await FSMClient.next()
+    await message.answer('Введіть адрес електронної пошти')
 
 
 async def email(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:  # работа со словарем машины состояний
-            # сохраняем в словарь машины состояния
-            data['Email'] = message.text
-        # тут надо бы все записать в БД
-        print('finish data', data)
-        await sql_add_command(state)  # записываем данные в бд
-        await message.answer('Новий запит успішно додано', reply_markup=action_menu_markup)
-        await state.finish()  # тут заканчивается машина состояний
+    # if message.from_user.id == ID:
+    async with state.proxy() as data:  # работа со словарем машины состояний
+        data['Email'] = message.text
+    print('finish data', data)
+    await sql_add_command(state)  # записываем данные в бд
+    await message.answer('Новий запит успішно додано', reply_markup=action_menu_markup)
+    await state.finish()  # тут заканчивается машина состояний
 
 
-async def param(message: types.Message):
+async def list_requests(message: types.Message):
     await message.reply('Список ваших запитів')
     await sql_read(message)
 
@@ -144,18 +138,18 @@ async def del_callback_run(callback_query: types.CallbackQuery):
 # добавляем кнопку удалить
 # @dp.message_handler(commands='Видалити запит')
 async def delete_item(message: types.Message):
-    if message.from_user.id == ID:  # проверка
-        # читаем из бд все
-        print('message', message)
-        read = await sql_read_for_del(message)  # читаем данные из бд
-        print('read', read)
-        for ret in read:
-            print('ret', ret)
-            # отправляем  данные и создаем кнопки для каждого запроса
-            await bot.send_message(message.from_user.id,
-                                   f'ДК021:2015: {ret[2]}\nСтатус: {ret[3]}\nВид закупівлі: {ret[4]}\nРегіон: {ret[5]}\nЧас відправки: {ret[6]}\nПошта: {ret[7]}',
-                                   reply_markup=InlineKeyboardMarkup().add(
-                                       InlineKeyboardButton('Видалити', callback_data=f'del {ret[0]}')))
+    # if message.from_user.id == ID:  # проверка
+    # читаем из бд все
+    print('message', message)
+    read = await sql_read_for_del(message)  # читаем данные из бд
+    print('read', read)
+    for ret in read:
+        print('ret', ret)
+        # отправляем  данные и создаем кнопки для каждого запроса
+        await bot.send_message(message.from_user.id,
+                               f'ДК021:2015: {ret[2]}\nСтатус: {ret[3]}\nВид закупівлі: {ret[4]}\nРегіон: {ret[5]}\nЧас відправки: {ret[6]}\nПошта: {ret[7]}',
+                               reply_markup=InlineKeyboardMarkup().add(
+                                   InlineKeyboardButton('Видалити', callback_data=f'del {ret[0]}')))
 
 
 def register_handlers_client(dp: Dispatcher):
@@ -175,7 +169,7 @@ def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(dispatch_time, state=FSMClient.Dispatch_time)
     dp.register_message_handler(email, state=FSMClient.Email)
 
-    dp.register_message_handler(param, Text(equals='Ваші запити', ignore_case=True))  # обычный текста
+    dp.register_message_handler(list_requests, Text(equals='Ваші запити', ignore_case=True))  # обычный текста
 
     dp.register_callback_query_handler(del_callback_run, lambda x: x.data and x.data.startswith('del '))
     dp.register_message_handler(delete_item, commands='Видалити запит')
