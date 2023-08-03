ccc79add9f7febffbd41bca6cf48ce4a715e4dbf
l-Spook-l <uaspookua@gmail.com>
2023-08-03 16:33:32 +0300

    Fixed an error where the number of tenders on the last page was calculated incorrectly

--- Parser/Prozorro_parser.py
+++ Parser/Prozorro_parser.py
@@ -18,64 +18,68 @@ async def get_json(data_for_parser):
 
     # print('url', get_url(data_for_parser))
     # url = "https://prozorro.gov.ua/api/search/tenders?filterType=tenders&status%5B0%5D=active.enquiries&status%5B1%5D=active.tendering&cpv%5B0%5D=71630000-3&cpv%5B1%5D=73110000-6&cpv%5B2%5D=50410000-2&page=1&region=1-6"
-    url = await get_url(data_for_parser)
+    url = await get_url(data_for_parser)  # надо со списком !!!!!!!!!!!!!!!!!!!!!!!!!
 
     async with aiohttp.ClientSession() as session:
-        # async with session.post(url=url, headers=headers) as response:
-        response = await session.post(url=url, headers=headers)
-        data = await response.json()
-        quantity_tender_in_page = data['per_page']
-        total_tenders = data["total"]
-        print('total_tenders -', total_tenders)
+        for i in range(len(url)):
+            print('url parser i', url[i])
+            # async with session.post(url=url, headers=headers) as response:
+            response = await session.post(url=url[i], headers=headers)
+            data = await response.json()
+            quantity_tender_on_page = data['per_page']
+            total_tenders = data["total"]
+            print('total_tenders -', total_tenders)
 
-        if total_tenders >= 500:
-            pass
+            if total_tenders >= 500:
+                pass
 
-        if total_tenders <= quantity_tender_in_page:
-            page = 1
-        else:
-            page = total_tenders // quantity_tender_in_page + 1
+            if total_tenders <= quantity_tender_on_page:
+                page = 1
+            else:
+                page = total_tenders // quantity_tender_on_page + 1
 
-        try:
-            for item in range(1, page + 1):
-                print(
-                    "==============================================================================================")
-                print(item)
-                # url_page = f"https://prozorro.gov.ua/api/search/tenders?filterType=tenders&status%5B0%5D=active.enquiries&status%5B1%5D=active.tendering&cpv%5B0%5D=71630000-3&cpv%5B1%5D=73110000-6&cpv%5B2%5D=50410000-2&page=1&region=1-6&page={item}"
-                url_page = f"{url}&page={item}"
+            try:
+                for item in range(1, page + 1):
+                    print(
+                        "==============================================================================================")
+                    print(item)
+                    # url_page = f"https://prozorro.gov.ua/api/search/tenders?filterType=tenders&status%5B0%5D=active.enquiries&status%5B1%5D=active.tendering&cpv%5B0%5D=71630000-3&cpv%5B1%5D=73110000-6&cpv%5B2%5D=50410000-2&page=1&region=1-6&page={item}"
+                    url_page = f"{url[i]}&page={item}"
 
-                async with session.post(url=url_page, headers=headers) as response_page:
-                    data_page = await response_page.json()
+                    async with session.post(url=url_page, headers=headers) as response_page:
+                        data_page = await response_page.json()
+                        print('quantity_tender_on_page', data_page["data"])
+                        print('data_page', len(data_page["data"]))
+                        for tender in range(len(data_page["data"])):
+                            title = data_page["data"][tender]["title"]
+                            try:
+                                city_company = data_page["data"][tender]["procuringEntity"]["address"]["locality"]
+                            except Exception as error:
+                                city_company = data_page["data"][tender]["procuringEntity"]["address"]["region"]
 
-                    for tender in range(quantity_tender_in_page):
-                        title = data_page["data"][tender]["title"]
-                        try:
-                            city_company = data_page["data"][tender]["procuringEntity"]["address"]["locality"]
-                        except Exception as error:
-                            city_company = data_page["data"][tender]["procuringEntity"]["address"]["region"]
-
-                        name_company = data_page["data"][tender]["procuringEntity"]["identifier"]["legalName"]
-                        ID_tender = data_page["data"][tender]["tenderID"]
-                        price = data_page["data"][tender]["value"]["amount"]
-                        try:
-                            start_date = data_page["data"][tender]["enquiryPeriod"]["startDate"][:10]
-                        except Exception as error:
-                            start_date = 'Дати проведення ще немає'
-                        link = (f"https://prozorro.gov.ua/tender/{ID_tender}")
-                        # print(
-                        #     "---------------------------------------------------------------------------------------")
-                        list_data += [[title, city_company, name_company, ID_tender, price, start_date, link]]
-                        # print(tender)
-                        # print(title, '\n', city_company, '\n', name_company, '\n', ID_tender,
-                        #       '\n', price, '\n', start_date, '\n', link)
-                        # print(
-                        #     "---------------------------------------------------------------------------------------")
-        except Exception as ex:
-            print('Something went wrong')
-            print(f'Error{ex}')
+                            name_company = data_page["data"][tender]["procuringEntity"]["identifier"]["legalName"]
+                            ID_tender = data_page["data"][tender]["tenderID"]
+                            price = data_page["data"][tender]["value"]["amount"]
+                            try:
+                                start_date = data_page["data"][tender]["enquiryPeriod"]["startDate"][:10]
+                            except Exception as error:
+                                start_date = 'Дати проведення ще немає'
+                            link = (f"https://prozorro.gov.ua/tender/{ID_tender}")
+                            # print(
+                            #     "---------------------------------------------------------------------------------------")
+                            list_data += [[title, city_company, name_company, ID_tender, price, start_date, link]]
+                            # print(tender)
+                            # print(title, '\n', city_company, '\n', name_company, '\n', ID_tender,
+                            #       '\n', price, '\n', start_date, '\n', link)
+                            # print(
+                            #     "---------------------------------------------------------------------------------------")
+            except Exception as ex:
+                print('Something went wrong')
+                print(f'Error{ex}')
 
     # finish_time = time() - start_time
     # print(f'Время затраченное {finish_time}')
+    print('list data', list_data)
     return list_data, total_tenders
 
 
