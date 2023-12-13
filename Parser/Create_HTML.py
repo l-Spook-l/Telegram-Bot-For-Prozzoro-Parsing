0d778739a6a9546886fc20193ea0d3c71685d10c
l-Spook-l <uaspookua@gmail.com>
2023-12-13 23:40:39 +0200

    Total number of tenders added to the email.

--- Parser/Create_HTML.py
+++ Parser/Create_HTML.py
@@ -13,14 +13,15 @@ async def create_HTML(data_for_parser):
                                '    <title>Document</title>\n', '</head>\n', '<body>\n', '\n', '</body>\n', '</html>']
 
         line = 0
+        List_HTML_for_email.insert(10 + line, f"<p>Загальна кількість тендерів: <span>{data[i]['total_tenders']}</span></p>\n")
         for item in range(data[i]['total_tenders']):
-            List_HTML_for_email.insert(10 + line, '<hr align="left" width="30%" color="black" size=1>\n')
-            List_HTML_for_email.insert(11 + line, f'<p><a href="{data[i]["list_tenders"][item][6]}">{data[i]["list_tenders"][item][0]}</a></p>\n')
-            List_HTML_for_email.insert(12 + line, f'<p>Місто: {data[i]["list_tenders"][item][1]}</p>\n')
-            List_HTML_for_email.insert(13 + line, f'<p><strong>Компанія: </strong>{data[i]["list_tenders"][item][2]}</p>\n')
-            List_HTML_for_email.insert(14 + line, f'<p><strong>ID: </strong>{data[i]["list_tenders"][item][3]}</p>\n')
-            List_HTML_for_email.insert(15 + line, f'<p>Очікувана вартість: <strong>{data[i]["list_tenders"][item][4]} UAH</strong></p>\n')
-            List_HTML_for_email.insert(16 + line, f'<p>Оголошено: {data[i]["list_tenders"][item][5]}</p>\n')
+            List_HTML_for_email.insert(11 + line, '<hr align="left" width="30%" color="black" size=1>\n')
+            List_HTML_for_email.insert(12 + line, f'<p><a href="{data[i]["list_tenders"][item][6]}">{data[i]["list_tenders"][item][0]}</a></p>\n')
+            List_HTML_for_email.insert(13 + line, f'<p>Місто: {data[i]["list_tenders"][item][1]}</p>\n')
+            List_HTML_for_email.insert(14 + line, f'<p><strong>Компанія: </strong>{data[i]["list_tenders"][item][2]}</p>\n')
+            List_HTML_for_email.insert(15 + line, f'<p><strong>ID: </strong>{data[i]["list_tenders"][item][3]}</p>\n')
+            List_HTML_for_email.insert(16 + line, f'<p>Очікувана вартість: <strong>{data[i]["list_tenders"][item][4]} UAH</strong></p>\n')
+            List_HTML_for_email.insert(17 + line, f'<p>Оголошено: {data[i]["list_tenders"][item][5]}</p>\n')
             line += 7
 
         async with aiofiles.open(f"index_{i + 1}.html", 'w', encoding='utf-8') as file:
